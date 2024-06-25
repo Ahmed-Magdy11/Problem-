930b432e4839b26a62d38ca339a4cf26979ee20b
Ahmed-Magdy11 <136619533+Ahmed-Magdy11@users.noreply.github.com>
2024-06-25 18:42:10 +0300

    Add files via upload

---
+++ Assignments/js/script.js
@@ -0,0 +1,283 @@
+// var x =prompt("Enter Your Number");
+// alert("Your Number is " +x)
+
+// var x =prompt("Enter Your Number");
+
+// if(x%3==0 && x%4==0){
+//     alert("YES")
+// }
+// else{
+//     alert("NO")
+// }
+
+
+// var x =prompt("Enter Your First Number");
+// var y =prompt("Enter Your Second Number");
+
+// if(x>y){
+//     alert(y);
+// }
+// else{
+//     alert(x)
+// }
+
+
+// var x =prompt("Enter Your Number");
+// if(x<0){
+//     alert("Negative");
+// }
+// else{
+//     alert("Positive");
+// }
+
+
+// var x =prompt("Enter Your First Number");
+// var y =prompt("Enter Your Second Number");
+// var z =prompt("Enter Your Third Number");
+// var mx=0, mn=1000000000;
+// if(x>mx){
+//     mx=x;
+// }
+// if(x<mn){
+//     mn=x;
+// }
+// if(y>mx){
+//     mx=y;
+// }
+// if(y<mn){
+//     mn=y;
+// }
+// if(z>mx){
+//     mx=z;
+// }
+// if(z<mn){
+//     mn=z;
+// }
+
+// alert("the max = "+mx +" and the min is = "+mn)
+
+
+
+// var x =prompt("Enter Your Number");
+
+// if(x%2==0 ){
+//     alert("Even")
+// }
+// else{
+//     alert("Odd")
+// }
+
+
+// var x=prompt("Enter Your letter ").toLocaleLowerCase();
+// if(x=="a" || x=='e' || x=="i" || "o" || x=="u"){
+//     alert("vowel");
+// }
+// else{
+//     alert("Consonant");
+// }
+
+
+// var x =prompt("Enter Your Number");
+// for(var i=0;i<=x;i++){
+//     console.log(i+" ");
+// }
+
+
+// var x =prompt("Enter Your Number");
+// for(var i=1;i<=12;i++){
+//     console.log(i*x);
+// }
+
+
+// var x =prompt("Enter Your Number");
+// for(var i=1;i<=x;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+// var x =prompt("Enter Your First Number");
+// var y =prompt("Enter Your Second Number");
+// var mul=1;
+// for(var i=0;i<y;i++){
+//     mul*=x;
+// }
+// console.log(mul);
+
+
+
+
+// var total=0;
+// for(var i=0;i<5;i++){
+
+//     var num =prompt("Enter Your Number");
+//     total+=num;
+
+// }
+// console.log("the total = " +total)
+// console.log("the average = " +total/5)
+// console.log("the Percentage = " +(total/500)*100)
+
+
+
+// var x=prompt("Enter Month Number");
+// if(x==1|| x==3||x==5||x==7||x==8||x==10|| x==12){
+//     alert("Month is 31 day");
+// }
+// else if(x==2){
+//     alert("Month is 29 day");
+// }
+// else{
+//     alert("Month is 30 day");
+
+// }
+
+
+
+// var x=prompt("Enter Your Grad");
+// var grad="";
+// if(x>=90){
+//     grad="A";
+// }
+// else if(x>=80){
+//     grad="B";
+// }
+// else if(x>=70){
+//     grad="C";
+// }
+// else if(x>=60){
+//     grad="D";
+// }
+// else if(x>=40){
+//     grad="E";
+// }
+// else{
+//     grad="F";
+// }
+// console.log(grad)
+
+
+
+
+
+// var x =prompt("Enter Number of Month ");
+
+// switch (x) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         alert("Month is 31 day");
+//         break;
+
+//     case 2:
+//         alert("Month is 29 day");
+//         break;
+
+//     default:
+//         alert("Month is 30 day");
+//         break;
+      
+// }
+
+
+
+// var x=prompt("Enter Letter ");
+// switch (x) {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         alert("Vowel");
+//         break;
+
+//     default:
+//         alert("Constant");
+//         break;
+// }
+
+
+
+
+
+
+// var x=prompt("Enter Your First number ");
+// var y=prompt("Enter Your Second number ");
+
+
+// switch (true) {
+//     case (x>y):
+//         alert(x);
+        
+//         break;
+
+//     default:
+//         alert(y);
+
+//         break;
+// }
+
+
+
+
+// var y=prompt("Enter You number ");
+
+
+// switch (true) {
+//     case (y%2==0):
+//         alert("Even");
+        
+//         break;
+
+//     default:
+//         alert("Odd");
+
+//         break;
+// }
+
+
+
+// var y=prompt("Enter You number ");
+
+
+// switch (true) {
+//     case (y>0):
+//         alert("Positive");
+//         break;
+//     case (y==0):
+//         alert("Zero");
+//         break;
+
+//     default:
+//         alert("Negative");
+
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
